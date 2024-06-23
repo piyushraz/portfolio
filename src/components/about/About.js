6885af2961cba7ca1e7bd7137e373be42ff7851e
Piyush Razdan <piyush.razdan@gmail.com>
2024-06-22 20:44:11 -0400

    Another commit

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -12,7 +12,7 @@ const About = () => {
           Hey there! I am Piyush, a student at the University of Toronto specializing in computer science. I like Python, Java, C, and Typescript. I am very open to learning new things and working on projects. Feel free to reach out at any time.
         </p>
         <br></br>
-        <p>Site and Resume: <a href="https://piyush.com">https://piyush.com</a></p>
+        <p>Site and Resume: <a href="https://piyush.com">Domain Incoming.</a></p>
         <p>GitHub: <a href="https://github.com/piyushraz">https://github.com/piyushraz</a></p>
         <p>LinkedIn: <a href="https://www.linkedin.com/in/piyushrazdan">https://www.linkedin.com/in/piyushrazdan</a></p>
       </div>
